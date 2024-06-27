7bdca6ad79840e2276649fb185ee19562f5fdcd8
gavin <gavin@taiko.xyz>
2024-06-27 18:08:51 +0800

    Revert "feat(txmgr): call `CreateAccessList` (#9)"
    This reverts commit 6873ecae1e909c433d59d00cade5989637c279ed.

--- op-service/txmgr/txmgr.go
+++ op-service/txmgr/txmgr.go
@@ -108,8 +108,6 @@ type ETHBackend interface {
 	// EstimateGas returns an estimate of the amount of gas needed to execute the given
 	// transaction against the current pending block.
 	EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error)
-	// CreateAccessList returns an estimate of the AccessList
-	CreateAccessList(ctx context.Context, msg ethereum.CallMsg) (*types.AccessList, uint64, string, error)
 	// Close the underlying eth connection
 	Close()
 }
@@ -266,21 +264,18 @@ func (m *SimpleTxManager) craftTx(ctx context.Context, candidate TxCandidate) (*
 
 	gasLimit := candidate.GasLimit
 
-	callArgs := ethereum.CallMsg{
-		From:      m.cfg.From,
-		To:        candidate.To,
-		GasTipCap: gasTipCap,
-		GasFeeCap: gasFeeCap,
-		Data:      candidate.TxData,
-		Value:     candidate.Value,
-	}
-
-	accessList, _, _, err := m.backend.CreateAccessList(ctx, callArgs)
-	if err != nil {
-		return nil, err
-	}
 	// If the gas limit is set, we can use that as the gas
 	if gasLimit == 0 {
+		callArgs := ethereum.CallMsg{
+			From:       m.cfg.From,
+			To:         candidate.To,
+			GasTipCap:  gasTipCap,
+			GasFeeCap:  gasFeeCap,
+			Data:       candidate.TxData,
+			Value:      candidate.Value,
+			AccessList: candidate.AccessList,
+		}
+
 		for _, blob := range candidate.Blobs {
 			commitment, err := blob.ComputeKZGCommitment()
 			if err != nil {
@@ -324,7 +319,7 @@ func (m *SimpleTxManager) craftTx(ctx context.Context, candidate TxCandidate) (*
 			Gas:        gasLimit,
 			BlobHashes: blobHashes,
 			Sidecar:    sidecar,
-			AccessList: *accessList,
+			AccessList: candidate.AccessList,
 		}
 		if err := finishBlobTx(message, m.chainID, gasTipCap, gasFeeCap, blobFeeCap, candidate.Value); err != nil {
 			return nil, fmt.Errorf("failed to create blob transaction: %w", err)
@@ -339,7 +334,7 @@ func (m *SimpleTxManager) craftTx(ctx context.Context, candidate TxCandidate) (*
 			Value:      candidate.Value,
 			Data:       candidate.TxData,
 			Gas:        gasLimit,
-			AccessList: *accessList,
+			AccessList: candidate.AccessList,
 		}
 	}
 	return m.signWithNextNonce(ctx, txMessage) // signer sets the nonce field of the tx
