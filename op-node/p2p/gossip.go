78a05def4127eaf0365b686713e5f6f3c23e2817
Jeffery Walsh <cyberhorsey@gmail.com>
2025-05-01 07:15:49 -0700

    bump

--- op-node/p2p/gossip.go
+++ op-node/p2p/gossip.go
@@ -485,7 +485,7 @@ func BuildPreconfBlocksResponseValidator(log log.Logger, cfg *rollup.Config, run
 			preconfblockLRU.Add(uint64(payload.BlockNumber), seen)
 		}
 
-		if count, _ := seen.hasSeen(payload.BlockHash); count > 5 {
+		if count, _ := seen.hasSeen(payload.BlockHash); count > 10 {
 			// [REJECT] if more than 5 blocks have been seen with the same block height
 			log.Warn("seen too many different blocks at same height", "height", payload.BlockNumber)
 			return pubsub.ValidationIgnore
